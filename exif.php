eb4c591fdd11393ac0d742a0f862cadf911394ac
Thomas Brouwer <tbrouwer44@gmail.com>
2013-04-03 16:05:15 +0300

    Update exif.php

--- exif.php
+++ exif.php
@@ -7,4 +7,19 @@
 
 $pic = 'your-picture.jpg';
 $exif = exif_read_data($pic, 'IFD0', 0);
+if ($exif) {
+  $height = $exif['COMPUTED']['height'];
+  $width = $exif['COMPUTED']['width'];
+  $make = $exif['make'];
+  $model = $exif['model'];
+  $exposure = $exif['ExposureTime'];
+  $isoSpeed = $exif['ISOSpeedRatings'];
+  $date = $exif['DateTimeOriginal'];
+  $flash = $exif['Flash'];
+  $focal = $exif['FocalLength'];
+  // ... more details about exif options with : var_dump($exif)
+} else {
+  echo 'exif unvailable';
+}
+
 ?>
