03e49dd3328c61f874286bac2015e92402d0c9c5
Antoine Turmel <geekshadow@gmail.com>
2013-04-04 11:14:14 +0300

    fix captcha.php, remove the form part.

--- captcha.php
+++ captcha.php
@@ -1,17 +1,17 @@
 <?php
 
-/* 
- * CAPTCHA.PHP
- * contain the generator of the captcha
- */ 
- 
+/*
+* CAPTCHA.PHP
+* contain the generator of the captcha
+*/
+
 session_start();
 $chars = 'abcdefghijklmnopqrstuvwxyz123456789';
 $size = 4; // can be change
 $captcha = '';
 
 // generate a random string with chars
-for ($i=0; $i<=4; $i++) {
+for ($i=0; $i<=$size; $i++) {
   $captcha .= $chars[rand(0, strlen($chars)-1)];
 }
 
@@ -19,38 +19,17 @@ for ($i=0; $i<=4; $i++) {
 $_SESSION['captcha'] = $captcha;
 
 // generate an image (70x30), you will need to change the size of the image if you change the size of the captcha
-$img = imagecreatetrue(70, 30);
+$img = imagecreatetruecolor(200, 100);
 $fill_color = imagecolorallocate($img, 255, 255, 255);
-imagefilledrectangle($img, 0, 0, 70, 30, $fill_color);
+imagefilledrectangle($img, 0, 0, 200, 100, $fill_color);
 
 // allocate color to the text and define the font (http://www.dafont.com)
 $txt_color = imagecolorallocate($img, 10, 10, 10);
 $font = 'yourfont.ttf';
-imagettftext($img, 23, 0, 5, 30), $txt_color, $font, $_SESSION['captcha'];
+imagettftext($img, 23, 0, 5, 30, $txt_color, $font, $_SESSION['captcha']);
 
 // print the image
 header("Content-type: image/jpeg");
 imagejpeg($img);
 imagedestroy($img);
 ?>
-
-<?php
-/* 
- * FORM.PHP
- * contain the form witch the captcha to valid
- */
- 
-if (isset($_POST['checkcaptcha'])) {
-  if ($_POST['captcha'] == $_SESSION['captcha']) {
-    echo 'okay';
-  } else {
-    echo 'error';
-  }
-}
-?>
-
-<form action="" method="post">
-  <img src="captcha.php" alt="the captcha" />
-  <input type="text" name="captcha" />
-  <input type="submit" value="Check captcha" name="checkcaptcha" />
-</form>
